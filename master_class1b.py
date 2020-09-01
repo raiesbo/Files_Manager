d1c821ad87b3ee8507e24d82b0524a12aa18ea01
raiesbo <raiesbo8@gmail.com>
2020-09-01 20:03:43 +0200

    new methods

--- master_class1b.py
+++ master_class1b.py
@@ -1,10 +1,16 @@
 # MASTER CLASS:
 import copy
 import os
+import json
+
+direc = "D:\\Usuarios\\Vicent Joan\\Documents\\REB\\Master_Class"
+
+with open(os.path.join(direc, "extensions.json")) as f:
+    extensions = json.load(f)
 
 
 class Folder:
-    def __init__(self, name, path="D:\\Usuarios\\Vicent Joan\\Documents\\REB\\Master_Class"):
+    def __init__(self, name, path=None):
         self.name = name
         self.path = path
         self.files = os.listdir(self.path)
@@ -12,10 +18,17 @@ class Folder:
     def create (self):
         pass
 
+    def new_folders(self):
+        for extention in extensions["python_project"]:
+            if extention not in self.files:
+                os.mkdir(direc + "\\" + extention)
 
-fd1 = Folder("Master_Class")
 
-print(fd1.files)
+folder1 = Folder("Master_Class", direc)
+
+print(folder1.files)
+
+folder1.new_folders()
 
 
 
