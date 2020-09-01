2cdb2d76e65fe84e429fe44d78851e036970caa4
raiesbo <raiesbo8@gmail.com>
2020-09-01 16:42:59 +0200

    refactor add files

--- master_class1b.py
+++ master_class1b.py
@@ -7,19 +7,13 @@ class Folder:
     def __init__(self, name, path="D:\\Usuarios\\Vicent Joan\\Documents\\REB\\Master_Class"):
         self.name = name
         self.path = path
-        self.files = []
+        self.files = os.listdir(self.path)
 
     def create (self):
         pass
 
-    def add_files(self):
-        files = os.listdir(self.path)
-        self.files.append(files)
-    
 
 fd1 = Folder("Master_Class")
-
-fd1.add_files()
 
 print(fd1.files)
 
